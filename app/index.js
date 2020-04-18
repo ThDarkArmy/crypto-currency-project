12075b1ef452ae83f6eaeb6e953b216f93a093c8
Prashant Raj <thepenseur1729@gmail.com>
2020-04-18 13:29:24 +0530

    added peer-to-peer server

--- app/index.js
+++ app/index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const Blockchain = require('../blockchain/index')
+const P2pServer = require('./p2p-server')
 
-const HTTP_PORT = process.env.PORT | 3001
+const HTTP_PORT = process.env.HTTP_PORT || 3001
 
 const app = express()
 
 app.use(bodyParser.json())
 
 const blockchain = new Blockchain();
+const p2pServer = new P2pServer(blockchain)
 
 app.get("/blocks", (req, res)=>{
     res.json(blockchain.chain);
@@ -21,3 +23,4 @@ app.post("/mine", (req, res)=>{
 })
 
 app.listen(HTTP_PORT, ()=>console.log("server is listening on port : "+ HTTP_PORT));
+p2pServer.listen();
